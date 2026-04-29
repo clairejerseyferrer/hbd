3c40e9c77b140aa4cc0cc7904f5696dab4ac4c49
clairejerseyferrer <clairejersey.ferrer@gmail.com>
2026-04-29 16:21:15 +0800

    Major Changes
    Removed About You, Added more features, Added Letters/Notes

--- script.js
+++ script.js
@@ -38,17 +38,46 @@ letterCards.forEach((card) => {
 const surpriseBtn = document.getElementById("surprise-btn");
 const surpriseMessage = document.getElementById("surprise-message");
 
-const messages = [
-    "🎂 You deserve the happiest birthday ever!",
-    "💖 Never forget how loved you are.",
-    "✨ May all your dreams come true.",
-    "🎁 Surprise! You are an amazing person!"
+const memories = [
+    {
+        type: "image",
+        src: "memory1.jpg",
+        text: "That time na niyaya kita sa nearby coffee shop XD that was our first meeting in a while after we went to different SHS"
+    },
+    {
+        type: "image",
+        src: "memory2.jfif",
+        text: "That time na I asked you pa what coffee you wanted kasi magvavalo tayo after my class XD"
+    },
+    {
+        type: "video",
+        src: "memory3.mp4",
+        text: "I saw this randomly and thought it's funny XD panahon na Kazuha glazers pa tayo and you set his voiceline as a ring tone XD"
+    }
+
 ];
+
+const surpriseImage = document.getElementById("surprise-image");
+const surpriseVideo = document.getElementById("surprise-video");
 
 surpriseBtn.addEventListener("click", () => {
 
-    const randomIndex = Math.floor(Math.random() * messages.length);
+    const randomIndex = Math.floor(Math.random() * memories.length);
+    const selected = memories[randomIndex];
 
-    surpriseMessage.textContent = messages[randomIndex];
+    // reset both first
+    surpriseImage.style.display = "none";
+    surpriseVideo.style.display = "none";
 
+    if (selected.type === "image") {
+        surpriseImage.src = selected.src;
+        surpriseImage.style.display = "block";
+    } else {
+        surpriseVideo.src = selected.src;
+        surpriseVideo.style.display = "block";
+        surpriseVideo.load();
+        surpriseVideo.play();
+    }
+
+    surpriseMessage.textContent = selected.text;
 });
